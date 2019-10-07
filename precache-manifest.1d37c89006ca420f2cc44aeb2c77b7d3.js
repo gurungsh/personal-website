1d1881035ba2eddbc7efc5f1960c0e333d1a3835
Shishir Gurung <gurungsh@gmail.com>
2019-10-07 02:02:36 -0500

    Updates

---
+++ precache-manifest.1d37c89006ca420f2cc44aeb2c77b7d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880157c8322cb08963f1a6f0cbcf1bfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddbb139a08453757f184",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "269889666d34af460ebf",
+    "url": "/static/css/main.aad63897.chunk.css"
+  },
+  {
+    "revision": "ddbb139a08453757f184",
+    "url": "/static/js/2.cee79313.chunk.js"
+  },
+  {
+    "revision": "269889666d34af460ebf",
+    "url": "/static/js/main.9f21160f.chunk.js"
+  },
+  {
+    "revision": "f4931a23c7c84b8428b0",
+    "url": "/static/js/runtime-main.baf32dfd.js"
+  },
+  {
+    "revision": "0a9a0de13303342a456114144392cecc",
+    "url": "/static/media/Shishir_Gurung_Resume.0a9a0de1.pdf"
+  },
+  {
+    "revision": "d2fca71ad037241b71f8f2a10ad66384",
+    "url": "/static/media/profile.d2fca71a.jpg"
+  }
+]);
